bfb00bee462e9995f334150b99a4be507fd263d6
Justin Goh <justin.goh@thoughtworks.com>
2019-12-07 23:57:09 +0800

    Initial commit

---
+++ Example/SwiftFramework/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftFramework
+//
+//  Created by jgch-tw on 12/07/2019.
+//  Copyright (c) 2019 jgch-tw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
