d91b3193e4ae939d1bba08cab856c0917d960125
jgch88 <radium.laboratories@gmail.com>
2019-12-08 00:51:47 +0800

    added private iosPods source and added ObjCPPFramework dependency on .podspec

--- Example/Pods/Target Support Files/Pods-SwiftFramework_Example/Pods-SwiftFramework_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-SwiftFramework_Example/Pods-SwiftFramework_Example-frameworks.sh
@@ -161,9 +161,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/ObjCPPFramework/ObjCPPFramework.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftFramework/SwiftFramework.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/ObjCPPFramework/ObjCPPFramework.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftFramework/SwiftFramework.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
