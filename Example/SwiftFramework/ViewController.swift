3432aedefb5a2de9b1493d399ad7cf3a84669117
jgch88 <radium.laboratories@gmail.com>
2019-12-08 01:12:25 +0800

    added button press

--- Example/SwiftFramework/ViewController.swift
+++ Example/SwiftFramework/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  SwiftFramework
-//
-//  Created by jgch-tw on 12/07/2019.
-//  Copyright (c) 2019 jgch-tw. All rights reserved.
-//
-
 import UIKit
 import SwiftFramework
 
@@ -13,14 +5,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var sr = SwiftRandomiser()
-        print(sr.generate())
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonPress(_ sender: UIButton) {
+        var sr = SwiftRandomiser()
+        print(sr.generate())
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 }
